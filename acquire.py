ac900a4b9dd1bc4cc1ce082d7c1b214af6f96c43
“Jen <Jen.j.eyring@gmail.com>
2022-05-12 16:46:30 -0500

    learning to prep

--- acquire.py
+++ acquire.py
@@ -7,19 +7,31 @@ import os
     
 def get_connection(db, user=env.user, host=env.host, password=env.password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+#_________________________________________________
+#iris_db from mysql 
 
 def get_iris_data():
-    filename = 'iris_db.csv'
+    '''
+    This function reads the iris data from the Codeup db into a df.
+    '''
+    sql_query = """
+                SELECT 
+                    species_id,
+                    species_name,
+                    sepal_length,
+                    sepal_width,
+                    petal_length,
+                    petal_width
+                FROM measurements
+                JOIN species USING(species_id)
+                """
 
-    if os.path.isfile(filename):
-        return pd.read_csv(filename)
-    else:
-        df = pd.read_sql('SELECT * FROM measurements JOIN species WITH species_id', get_connection('iris_db'))
-        
-        df.to_csv(filename)
+    # Read in DataFrame from Codeup db.
+    df = pd.read_sql(sql_query, get_connection('iris_db'))
 
-        return df
-
+    return df
+#_________________________________________________________________________________
+#google sheet 'Telco churn data'
 
 sheet_url = 'https://docs.google.com/spreadsheets/d/1zozkbdAJBIovrqqaE7k0rC2dlKcaI9sA7BsSnJ7eAtg/edit#gid=1023018493'    
 
@@ -33,18 +45,48 @@ def google_sheets():
     csv_export_url = sheet_url.replace('/edit#gid=', '/export?format=csv&gid=')
     df_googlesheet = pd.read_csv(csv_export_url)
     return df_googlesheet
+#__________________________________________________________________________________
+#Excel version of Telco churn data
 
 def excel_file():
     excel_sheet="Jennifer Eyring - jemison_spreadsheet_exercises.xlsx"
     df_excel = pd.read_excel(excel_sheet)
     return df_excel
 
+#_________________________________________________________________________________
+#Titanic data
+
+def new_titanic_data():
+    '''
+    This function reads the titanic data from the Codeup db into a df,
+    write it to a csv file, and returns the df.
+    '''
+    # Create SQL query.
+    sql_query = 'SELECT * FROM passengers'
+    
+    # Read in DataFrame from Codeup db.
+    df = pd.read_sql(sql_query, get_connection('titanic_db'))
+    
+    return df
+
+
 
 def get_titanic_data():
-    new_titanic_data=
+    '''
+    This function reads in titanic data from Codeup database, writes data to
+    a csv file if a local file does not exist, and returns a df.
+    '''
     if os.path.isfile('titanic_df.csv'):
+        
+        # If csv file exists, read in data from csv file.
         df = pd.read_csv('titanic_df.csv', index_col=0)
+        
     else:
+        
+        # Read fresh data from db into a DataFrame.
         df = new_titanic_data()
-
+        
+        # Write DataFrame to a csv file.
         df.to_csv('titanic_df.csv')
+        
+    return df
